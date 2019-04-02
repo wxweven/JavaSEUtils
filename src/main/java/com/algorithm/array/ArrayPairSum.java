4dc75a82c4032732eb38742be21085f128dcdc1f
wxweven <wxweven@gmail.com>
2019-04-03 00:24:33 +0800

    add array sum

---
+++ src/main/java/com/algorithm/array/ArrayPairSum.java
@@ -0,0 +1,26 @@
+package com.algorithm.array;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author wxweven
+ * @date 2019/2/19
+ */
+public class ArrayPairSum {
+    @Test
+    public void test(){
+        int res = arrayPairSumSoultion(new int[]{4,2,1,3});
+        System.out.println(res);
+    }
+
+    public static int arrayPairSumSoultion(int[] arr){
+        Arrays.sort(arr);
+        int result = 0;
+        for(int i=0; i<arr.length;i+=2){
+            result += arr[i];
+        }
+        return result;
+    }
+}
