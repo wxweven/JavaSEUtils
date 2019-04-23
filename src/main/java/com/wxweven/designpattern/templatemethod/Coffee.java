de46fc3701881856c02b42013e4eda20124ca8a7
wangxw03 <wangxw03@fenbi.com>
2019-04-23 21:16:25 +0800

    add template method pattern

---
+++ src/main/java/com/wxweven/designpattern/templatemethod/Coffee.java
@@ -0,0 +1,11 @@
+package com.wxweven.designpattern.templatemethod;
+
+public class Coffee extends CaffeineBeverage {
+    public void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    public void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
+}
